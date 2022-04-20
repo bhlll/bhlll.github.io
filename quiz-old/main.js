5513e994b2376995cf9f6443d0df0c01b3b609d9
bhlll <binghui.liu52@gmail.com>
2022-04-20 11:56:20 +0300

    try quiz with image

--- quiz-old/main.js
+++ quiz-old/main.js
@@ -4,58 +4,90 @@ const buildQuiz = () => {
 
   // for each question...
   questions.forEach((currentQuestion, questionNumber) => {
-      // variable to store the list of possible answers
-      const answers = [];
+    // variable to store the list of possible answers
+    const answers = [];
 
-      // and for each available answer...
-      currentQuestion.answers.forEach((answer, index) => {
+    // and for each available answer...
+    currentQuestion.answers.forEach((answer, index) => {
 
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value=${index}>
+      // ...add an HTML radio button
+      answers.push(
+        `<label>
+            <input id = "question${questionNumber}" type="radio" name="question${questionNumber}" value=${index}>
             ${answer} 
           </label>`
-        );
-      })
+      );
+    })
 
-      // add this question and its answers to the output
-      output.push(
-        `<div class="slide">
+    // add this question and its answers to the output
+    output.push(
+      `<div class="slide">
           <div class="question"> ${currentQuestion.text} </div>
           <div class="answers"> ${answers.join("")} </div>
         </div>`
-      );
-    }
+    );
+  }
   );
 
   // finally combine our output list into one string of HTML and put it on the page
   document.getElementById('quiz').innerHTML = output.join('');
 }
 
+
+const showPage = (n) => {
+  pages[currentPage].classList.remove('active-page');
+  pages[n].classList.add('active-page');
+  currentPage = n;
+}
+const showNextPage = () => {
+  // saveAsPng
+  showPage(currentPage + 1);
+}
+
+const startAction = ()=>{
+  // console.log('a')
+  showNextPage()
+  showSlide(0)
+}
+
 const showSlide = (n) => {
   slides[currentSlide].classList.remove('active-slide');
   slides[n].classList.add('active-slide');
   currentSlide = n;
-  if(currentSlide === 0){
+  if (currentSlide === 0) {
     previousButton.style.display = 'none';
-  }
-  else{
+  } else {
     previousButton.style.display = 'inline-block';
   }
-  if(currentSlide === slides.length - 1){
+  if (currentSlide === slides.length - 1) {
     nextButton.style.display = 'none';
     submitButton.style.display = 'inline-block';
   }
-  else{
+  else {
     nextButton.style.display = 'inline-block';
     submitButton.style.display = 'none';
   }
 }
 
+
 const showNextSlide = () => {
   console.log("show next");
-  showSlide(currentSlide + 1);
+  // console.log("question"+currentSlide)
+  const currQ = document.getElementsByName("question"+currentSlide)
+  // querySelector('input[name="question${currentSlide}"]:checked').value
+  // .getElementById('question'+(currentSlide))
+  console.log(currQ)
+  let selected  = false;
+  currQ.forEach(element => {
+    if(element.checked){
+      selected = true
+    }
+  });
+  if(selected){
+    showSlide(currentSlide + 1);
+  }else{
+    window.alert('你还没有选择答案哟')
+  }
 }
 
 const showPreviousSlide = () => {
@@ -80,7 +112,7 @@ const calcuateResult = (input) => {
     F: 0,
     G: 0,
   }
-  input.forEach((value, index)=> {
+  input.forEach((value, index) => {
     const questionScore = scores[index].score[value];
     results[questionScore.result] += questionScore.point;
   });
@@ -104,6 +136,7 @@ const maxResult = (dict) => {
 
 const showResults = () => {
   // gather answer containers from our quiz
+  showNextPage();
   const answerContainers = quizContainer.querySelectorAll('.answers');
 
   const answers = [];
@@ -112,25 +145,92 @@ const showResults = () => {
     const answerContainer = answerContainers[questionNumber];
     const selector = `input[name=question${questionNumber}]:checked`;
     const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    answerContainer.querySelector(selector).checked = false;
     answers.push(userAnswer);
   });
 
-  alert(calcuateResult(answers));
+
+  let finalAnswer = calcuateResult(answers)
+  
+  const result = [];
+  console.log(username)
+  result.push(
+
+
+    ` 
+    <div>${username.value}</div>
+     <div style="margin-top:30%"> ${finalAnswer} </div>
+     <div class = "result-block">
+     <div class = "description"></div>
+     <div class = "result-img" style = ""></div>
+    </div>
+`
+  );
+
+  document.getElementById('results').innerHTML = result.join('');
+
+}
+
+const backToHome = ()=>{
+  // currentPage = 0;
+  showPage(0)
+  // buildQuiz()
+  showSlide(0)
+
+  console.log(currentPage)
+  // if (currentPage !== 4) {
+  //   saveButtons.style.display = 'none';
+  // }
+  // console.log(currentPage)
+}
+
+const savePoster = ()=>{
+  console.log('save poster')
+  const resultPoster = document.getElementById('resultPoster');
+  saveAsPng(node, { filename: 'Report', printDate: true });
+
+}
+
+const showPoster = ()=>{
+showNextPage();
+results.push(
+  `<div>
+  <div></>
+    </div>`
+);
 }
 
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
-buildQuiz();
+// buildQuiz();
+buildQuiz()
 
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
 const slides = document.querySelectorAll(".slide");
+const pages = document.querySelectorAll(".page");
 let currentSlide = 0;
-showSlide(currentSlide);
+// showSlide(currentSlide);
+
+const startButton = document.getElementById("start-btn");
+const backToHomeButton = document.getElementById("backToHomeButton");
+const showPosterButton = document.getElementById("showPosterButton");
+const saveButton = document.getElementById("saveButton");
+const saveButtons = document.getElementById("saveButtons")
+const username = document.getElementById("username")
+let currentPage = 0;
+
+showPage(currentPage);
+
+
 
 // Event listeners
 submitButton.addEventListener('click', showResults);
 previousButton.addEventListener("click", showPreviousSlide);
 nextButton.addEventListener("click", showNextSlide);
+startButton.addEventListener("click", startAction);
+showPosterButton.addEventListener("click",showNextPage);
+backToHomeButton.addEventListener("click", backToHome)
+saveButton.addEventListener("click", savePoster)
